deca1eddf99080545f18c3346df2c180975948f4
megb92 <97361342+megb92@users.noreply.github.com>
2022-04-08 11:04:10 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,50 @@
+var video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	video.autoplay = false;
+	video.loop = false;
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate *= 0.95;
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up");
+	video.playbackRate /= .95;
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	if (video.currentTime < video.duration-15){
+		console.log("original time " + video.currentTime)
+		video.currentTime += 15;
+		console.log("new time " + video.currentTime)
+	}
+	else {
+		video.currentTime = 0;
+		video.play;
+		console.log("new time " + video.currentTime)
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute");
+	video.
+});
